8e6fc8cfedf02079d209e70209fb059e9a912e4c
Dunatron <heath.dunlop.hd@gmail.com>
2017-10-17 02:11:00 +1300

    fff

--- themes/react-app/dist/app.bundle.js
+++ themes/react-app/dist/app.bundle.js
@@ -21889,7 +21889,7 @@ exports = module.exports = __webpack_require__(15)(undefined);
 
 
 // module
-exports.push([module.i, ".App {\n  text-align: center;\n}\n\n.App-logo {\n  animation: App-logo-spin infinite 20s linear;\n  height: 80px;\n}\n\n.ss-logo {\n  height: 80px;\n}\n\n.App-header {\n  background-color: #222;\n  height: 150px;\n  padding: 20px;\n  color: white;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n}\n\n.App-title {\n  font-size: 1.5em;\n}\n\n.App-intro {\n  font-size: large;\n}\n\n@keyframes App-logo-spin {\n  from { transform: rotate(0deg); }\n  to { transform: rotate(360deg); }\n}\n\n.Main__Body {\n  text-align: center;\n}\n", ""]);
+exports.push([module.i, ".App {\n  text-align: center;\n}\n\n.App-logo {\n  animation: App-logo-spin infinite 20s linear;\n  height: 80px;\n}\n\n.ss-logo {\n  height: 80px;\n}\n\n.App-header {\n  background-color: #222;\n  min-height: 150px;\n  height: auto;\n  padding: 20px;\n  color: white;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  flex-wrap: wrap;\n}\n\n.App-title {\n  font-size: 1.5em;\n}\n\n.App-intro {\n  font-size: large;\n}\n\n@keyframes App-logo-spin {\n  from { transform: rotate(0deg); }\n  to { transform: rotate(360deg); }\n}\n\n.Main__Body {\n  text-align: center;\n}\n", ""]);
 
 // exports
 
